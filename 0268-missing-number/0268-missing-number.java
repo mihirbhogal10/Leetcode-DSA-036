5c5d971bba449b9bc6bac90a43a29bef290fc991
mihirbhogal10 <mihir.bhogal@gmail.com>
2026-06-18 13:22:50 +0530

    Time: 0 ms (100%), Space: 47.2 MB (64.76%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int x = nums.length; 
+        for (int i = 0; i < nums.length; i++) {
+            x = x ^ i ^ nums[i];
+        }
+        return x;
+    }
+}
